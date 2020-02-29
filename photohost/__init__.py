0f7ccbce4225def9a1d58bd1b28b8d8c2a5842e0
Theo Henson <theodorehenson@protonmail.com>
2020-02-29 18:38:18 -0500

    Added filler content

--- photohost/__init__.py
+++ photohost/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request
-app = Flask(__name__, static_folder='static', static_url_path='')
+app = Flask(__name__, static_folder="static")
 
 @app.route("/")
 def index():
