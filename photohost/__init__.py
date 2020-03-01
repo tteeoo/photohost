ee87b072bfcf025229644212fed8e6fad841c9c9
Theo Henson <theodorehenson@protonmail.com>
2020-03-01 00:23:17 -0500

    Edited viewing

--- photohost/__init__.py
+++ photohost/__init__.py
@@ -6,6 +6,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__, static_folder="static")
 app.config["UPLOAD_FOLDER"] = "/home/theo/test"
 app.config["MAX_CONTENT_LENGTH"] = 32 * 1024 * 1024
+app.secret_key = os.urandom(16)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -16,10 +17,10 @@ def allowed_file(filename):
 def index():
     if request.method == "POST":
         if "file" not in request.files:
-            return "No file uploaded"
+            return redirect(request.url)
         file = request.files["file"]
         if file.filename == "":
-            return "No selected file"
+            return redirect(request.url)
         if allowed_file(file.filename) == False:
             return "Invalid file type"
         if file:
@@ -29,15 +30,17 @@ def index():
             with open(tmpname, "rb") as f:
                 filename = sha256(f.read()).hexdigest() + extension
             os.rename(tmpname, os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            url = "https://photohost.tech/view" + filename
                 
-            return filename
+            return redirect(url)
         else:
             return "An unknown error occurred"
     return render_template("index.html")
 
-@app.route("/<image>")
+@app.route("/image/<filename>")
+def return_image(filename):
+    return send_from_directory("/home/theo/test/", filename, as_attachment=True)
+
+@app.route("/view/<image>")
 def view(image):
-    try:
-        return send_from_directory("/home/theo/test/", filename=image)
-    except FileNotFoundError:
-        abort(404)
+    return render_template("view.html", image=image)
