fdf4e66cf2a705937a355d4703ce3e14275022f5
Theo Henson <theodorehenson@protonmail.com>
2020-03-02 19:15:33 -0500

    Added multi-file support

--- photohost/__init__.py
+++ photohost/__init__.py
@@ -1,4 +1,7 @@
 import os
+import shutil
+import zipfile
+import pathlib
 from hashlib import sha256
 from flask import Flask, render_template, url_for, request, flash, redirect, abort, send_from_directory
 from werkzeug.exceptions import HTTPException
@@ -12,7 +15,7 @@ app.secret_key = os.urandom(16)
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in \
-            ["png", "jpg", "jpeg", "gif", "ico", "tif", "svg"]
+            ["png", "jpg", "jpeg", "gif", "ico", "tif", "svg", "zip"]
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -25,13 +28,28 @@ def index():
         if allowed_file(file.filename) == False:
             return render_template("error.html", errno="Error: Invalid file type, please upload an image.")
         if file:
-            tmpname = os.path.join(app.config["UPLOAD_FOLDER"]+"/tmp", secure_filename(file.filename))
             extension = "." + file.filename.split(".")[1]
-            file.save(tmpname)
-            with open(tmpname, "rb") as f:
-                filename = sha256(f.read()).hexdigest() + extension
-            os.rename(tmpname, os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            url = "https://photohost.tech/view/" + filename
+            if extension != ".zip": 
+                tmpname = os.path.join(app.config["UPLOAD_FOLDER"]+"/tmp", secure_filename(file.filename))
+                file.save(tmpname)
+                with open(tmpname, "rb") as f:
+                    filename = sha256(f.read()).hexdigest() + extension
+                os.rename(tmpname, os.path.join(app.config["UPLOAD_FOLDER"], filename))
+                url = "https://photohost.tech/view/" + filename
+            else:
+                zip = zipfile.ZipFile(file)
+                for i in zip.namelist()[1:]:
+                    if allowed_file(i) == False:
+                        return render_template("error.html", errno="Error: Invalid file type inside zip archive, please only upload images.")
+                tmpname = os.path.join(app.config["UPLOAD_FOLDER"]+"/tmp", secure_filename(file.filename))
+                file.save(tmpname)
+                with open(tmpname, "rb") as f:
+                    foldername = "/"+sha256(f.read()).hexdigest()
+                pathlib.Path(app.config["UPLOAD_FOLDER"]+foldername).mkdir(exist_ok=True)
+                zip.extractall(app.config["UPLOAD_FOLDER"]+"/tmp")
+                os.remove(tmpname)
+                os.rename(os.path.join(app.config["UPLOAD_FOLDER"]+"/tmp", file.filename.split(".")[0]+"/"), app.config["UPLOAD_FOLDER"]+foldername+"/")
+                url = "https://photohost.tech/multi" + foldername
                 
             return redirect(url)
         else:
@@ -42,21 +60,33 @@ def index():
 def return_image(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename, as_attachment=False)
 
+@app.route("/multi/<folder>/<filename>")
+def return_image_folder(folder, filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"]+"/"+folder, filename, as_attachment=False)
+
 @app.route("/view/<image>")
 def view(image):
     return render_template("view.html", image=image)
+
+@app.route("/multi/<folder>")
+def multi(folder):
+    try:
+        images = os.listdir(app.config["UPLOAD_FOLDER"]+"/"+folder)
+    except FileNotFoundError:
+        abort(404)
+    return render_template("multi.html", folder=folder, images=images)
 
 @app.route('/robots.txt')
 @app.route('/sitemap.xml')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
-@app.errorhandler(Exception)
-def error(e):
-    code = 500
-    if isinstance(e, HTTPException):
-        code = e.code
-    return render_template("error.html", errno="HTTP Error: "+str(code))
+#@app.errorhandler(Exception)
+#def error(e):
+    #code = 500
+    #if isinstance(e, HTTPException):
+        #code = e.code
+    #return render_template("error.html", errno="HTTP Error: "+str(code))
 
 if __name__ == "__main__":
     app.run(debug=True)
